3468254c4c301f86bd47b6cf3547193a0b8c447e
Bryan Mills <bryan@millz.net>
2018-04-23 20:11:26 -0400

    move site elements under vellum folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,6 @@ const IndexPage = ({data}) => {
     const component = data.component;
 
     return (
-        <section style={{width: "840px", margin: "0 auto"}}>
-            <Inkwell meta={component}>
-                <EventCard title="Vellum Hackathon"
-                           date="February 1, 2018"
-                           location="Philadelphia, PA"
-                           startTime="10:00 AM"
-                           endTime="6:00 PM"
-                           image="https://picsum.photos/320/180">
-                    Join us, and other local developers to learn all about using
-                    Vellum to build your next web site. This is a hands-on seminar,
-                    so bring your own laptop!
-                </EventCard>
-            </Inkwell>
-        </section>
     );
 };
 
